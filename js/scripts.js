e745c7cd671cbcef6b7c2a77e5ea6bc8d1c15d91
PrincessMooMoo <chriscowendy@gmail.com>
2018-01-18 18:44:43 -0800

    day 1 in class work

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
  console.log("Hello World!!!");
+//I am a single-line comment
+/*I am a a multi-line comment
+and I don't care*/
+/*alwaysUseCamelCase naming convention*/
+
+/*scoped indenting:
+var myFunction = function(argument) {
+      console.log(this);
+  }
+*/
+//Always end a statement with a semi-colon!
